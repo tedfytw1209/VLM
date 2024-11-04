24866f790f494e836073d712dfa8182cd8f3b0b7
Mingxin Zheng <18563433+mingxin-zheng@users.noreply.github.com>
2024-11-04 23:41:09 +0800

    Add persistence to parameters and clean up (#50)
    This PR:
    - cleans up some code in the demo
    - adds persistence to `top_p`, `temperature`, and `max_tokens` in one
    conversation
    
    ---------
    
    Signed-off-by: Mingxin Zheng <mingxinz@nvidia.com>

--- m3/demo/gradio_m3.py
+++ m3/demo/gradio_m3.py
@@ -304,31 +304,24 @@ def new_session_variables(**kwargs):
 class M3Generator:
     """Class to generate M3 responses"""
 
-    def __init__(self, source="huggingface", model_path="", conv_mode=""):
+    def __init__(self, source="huggingface", model_path="MONAI/Llama3-VILA-M3-8B", conv_mode="llama_3"):
         """Initialize the M3 generator"""
         global SYS_PROMPT
         self.source = source
-        # TODO: support huggingface models
-        if source == "local":
+        if source == "local" or source == "huggingface":
             # TODO: allow setting the device
             disable_torch_init()
             self.conv_mode = conv_mode
+            if source == "huggingface":
+                model_path = snapshot_download(model_path)
             model_name = get_model_name_from_path(model_path)
             self.tokenizer, self.model, self.image_processor, self.context_len = load_pretrained_model(
                 model_path, model_name
             )
             logger.info(f"Model {model_name} loaded successfully. Context length: {self.context_len}")
-        elif source == "huggingface":
-            model_path = snapshot_download(model_path)
-            self.conv_mode = conv_mode
-            model_name = get_model_name_from_path(model_path)
-            self.tokenizer, self.model, self.image_processor, self.context_len = load_pretrained_model(
-                model_path, model_name
-            )
-            logger.info(f"Model {model_name} loaded successfully. Context length: {self.context_len}")
+            SYS_PROMPT = conv_templates[self.conv_mode].system  # only set once
         else:
             raise NotImplementedError(f"Source {source} is not supported.")
-        SYS_PROMPT = conv_templates[self.conv_mode].system  # only set once
 
     def generate_response_local(
         self,
@@ -429,7 +422,7 @@ class M3Generator:
                     j += 1
                 del messages[i + 1 : j]  # Remove all the squashed expert messages
 
-            i += 1  # Move to the next message. TODO: Check if this is correct
+            i += 1
 
         return messages
 
@@ -537,6 +530,9 @@ class M3Generator:
             use_model_cards=sv.use_model_cards,
             download_file_path=download_pkg,
             temp_working_dir=sv.temp_working_dir,
+            max_tokens=sv.max_tokens,
+            temperature=sv.temperature,
+            top_p=sv.top_p,
             interactive=True,
             sys_msgs_to_hide=sv.sys_msgs_to_hide,
             image_url_backup=sv.image_url,
